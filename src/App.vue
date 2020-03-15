8b426eb30d0b59aad5d5827e44b21a594b7428e4
Siphamandla brian Mjoli <g14m1190@campus.ru.ac.za>
2020-03-15 15:21:46 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -104,7 +104,30 @@ export default {
         this.init()
     },
     methods: {
+        preventPageInspect() {
+            document.addEventListener('contextmenu', function (e) {
+                e.preventDefault();
+            });
+            document.onkeydown = function (e) {
+                if (event.keyCode == 123) {
+                    return false;
+                }
+                if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+                    return false;
+                }
+                if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+                    return false;
+                }
+                if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+                    return false;
+                }
+                if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+                    return false;
+                }
+            }
+        },
         init: async function () {
+            this.preventPageInspect()
             var registry = await Promise.resolve(this.getRegistry())
             console.log('registry: ', registry)
             this.$store.state.recentVideos = registry
@@ -123,10 +146,11 @@ export default {
                 })
             })
         },
-        warn(){
-          Swal.fire({icon:'warning',
-          text:"Website under development",
-          })
+        warn() {
+            Swal.fire({
+                icon: 'warning',
+                text: "Website under development",
+            })
         }
     },
 }
--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     registryURL:'https://api.myjson.com/bins/odagq',
-    thumbnail: '../images/thumbnail_blank.jpg',
+    thumbnail: './images/thumbnail_blank.jpg',
     popularVideos: [
     ],
     recentVideos: [],
